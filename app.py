d75727955a3baf04a1aefeb72af148bbe44f872f
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2023-08-23 18:29:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,30 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
-st.header("🏠 AI Scout Solutions - Real Estate Description Generator Demo")
+st.header("AI Scout Solutions- NLU Utterance Generator")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 st.sidebar.subheader('About')
-st.sidebar.caption('Created by Tony at AI Scout Solutions. For demo purposes only- this is not the final product.')
+st.sidebar.caption('Created by Tony at AI Scout Solutions.')
 
-def real_estate_description(location, property_type, bedrooms, bathrooms, special_features, price):
+def utterance_gen(location, property_type, bedrooms, bathrooms, special_features, price):
     with st.spinner("Generating real estate description..."):
         # Instantiate LLM model
         llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
         # Prompt
-        template = "Generate a real estate description for a {property_type} located in {location}. It has {bedrooms} bedrooms and {bathrooms} bathrooms. Special features include: {special_features}. The listing price is {price}."
+        template = """I have several intents setup for this question:
+
+                    {question}
+
+                    The intents are: {intents}
+
+
+                    Generate 50 variations in which a user may respond for each intent. Keep in mind this is a chatbot, so users may not write in full sentences or use correct spelling/grammar at all times. For example, a user may say, "{example}""""
         prompt_query = template.format(
-            property_type=property_type,
-            location=location,
-            bedrooms=bedrooms,
-            bathrooms=bathrooms,
-            special_features=special_features,
-            price=price
+            question=question,
+            intents=intents,
+            example=example,
         )
         # Run LLM model
         response = llm(prompt_query)
@@ -29,17 +33,14 @@ def real_estate_description(location, property_type, bedrooms, bathrooms, specia
         return st.info(response)
 
 with st.form("myform"):
-    location = st.text_input("Enter the property location (e.g., city, neighborhood):", "")
-    property_type = st.text_input("Enter the property type (e.g., house, apartment):", "")
-    bedrooms = st.text_input("Enter the number of bedrooms:", "")
-    bathrooms = st.text_input("Enter the number of bathrooms:", "")
-    special_features = st.text_input("Enter any special features or highlights (e.g., pool, modern kitchen, view):", "")
-    price = st.text_input("Enter the listing price or rental price:", "")
+    question = st.text_input("Please enter the question:", "")
+    intents = st.text_input("Please enter all intents for this question, comma-separated:", "")
+    example = st.text_input("Please enter an example utterance for ONE of the intents:", "")
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
         st.info("Please add your OpenAI API key in the sidebar to continue.")
     elif submitted:
-        real_estate_description(location, property_type, bedrooms, bathrooms, special_features, price)
+        utterance_gen(location, property_type, bedrooms, bathrooms, special_features, price)
 
 
 hide_streamlit_style = """
